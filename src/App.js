9048e91e0b0a85c9336427fd62b7f382748cdf09
Dani <danihazler@gmail.com>
2018-12-12 22:51:42 +0000

    Add animation libraries // Make tests w/ it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Intro from './components/sections/intro/Intro';
 import Showreel from './components/sections/showreel/Showreel';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    AOS.init();
+  }
+
   render() {
     return (
       <div className="App">
--- src/components/sections/showreel/Showreel.js
+++ src/components/sections/showreel/Showreel.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
+import posed from 'react-pose';
 import './Showreel.css';
 
 import blackLogo from "../../../assets/images/black_logo.png";
 import showVideo from "../../../assets/videos/showreel.mp4";
 
-const Showreel = () => {
-  return (
-    <section className="showreel">
-      <div className="curtain flex f-column">
-        <div className="navbar flex">
-          <div className="nav-logo">
-            <img src={blackLogo} alt="Lagardere Logo" />
+const LogoAnime = posed.img({
+  hidden: {opacity: 0},
+  visible: {opacity:1}
+})
+
+class Showreel extends Component {
+  state = {
+    isVisible: false
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        isVisible: !this.state.isVisible
+      })
+    }, 1000)
+  }
+
+  render(){
+    return (
+      <section className="showreel">
+        <div className="curtain flex f-column">
+          <div className="navbar flex">
+            <div className="nav-logo"
+              data-aos="slide-right"
+              data-aos-duration="3000"
+            >
+              <LogoAnime src={blackLogo} alt="Lagardere Logo" />
+            </div>
+            <div className="_header">
+              <nav className="flex">
+                <div className="nav-menu">
+                  Menu
+                </div>
+                <div className="nav-burger">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </nav>
+            </div>
           </div>
-          <div className="_header">
-            <nav className="flex">
-              <div className="nav-menu">
-                Menu
-              </div>
-              <div className="nav-burger">
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </nav>
-          </div>
+          <video id="showreel" width="720" height="480" controls>
+            <source src={showVideo} type="video/mp4" />
+              Your browser does not support HTML5.
+          </video>
         </div>
-        <video id="showreel" width="720" height="480" controls>
-          <source src={showVideo} type="video/mp4" />
-            Your browser does not support HTML5.
-        </video>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
 
 export default Showreel;
